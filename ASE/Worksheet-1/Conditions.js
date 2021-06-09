3bc450845f84cf523ed295128690d362cd06137f
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-09 13:23:08 +0100

    Create Conditions.js

---
+++ ASE/Worksheet-1/Conditions.js
@@ -0,0 +1,13 @@
+function checkNumber(myArgument)
+{
+    if (myArgument == 42){
+        console.log('correct');
+    }
+    else {
+        console.log('incorrect');
+    }
+}
+
+checkNumber(3);
+checkNumber(7);
+checkNumber(42);
