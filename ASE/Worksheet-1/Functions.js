55e73ca4ede1e5fd612edc865ee4598013adbed6
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-09 13:26:07 +0100

    Create Functions.js

---
+++ ASE/Worksheet-1/Functions.js
@@ -0,0 +1,10 @@
+// https://www.learn-js.org/en/Functions
+
+function multiplyFive(num){
+    return num * 5
+}
+
+console.log(multiplyFive(3));
+console.log(multiplyFive(4));
+console.log(multiplyFive(5));
+console.log(multiplyFive(6));
