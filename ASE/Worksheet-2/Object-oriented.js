2745c9c5c972f0ca87f3964240cdb863c4be50f3
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-10 10:52:05 +0100

    Update Object-oriented.js

--- ASE/Worksheet-2/Object-oriented.js
+++ ASE/Worksheet-2/Object-oriented.js
@@ -1,9 +1,14 @@
 // https://www.learn-js.org/en/Object_Oriented_JavaScript
 
-var Person = function(name, age) {
+var Person = function(name, age){
     this.name = name;
     this.age = age;
-    this.describe = function() {
+    this.describe = function(){
         return this.name + ", " + this.age + " years old";
     };
 };
+
+var jack = new Person("Jack", 25);
+var jill = new Person("Jill", 24);
+console.log(jack.describe());
+console.log(jill.describe());
