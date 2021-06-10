a866b37cc270fae2f3f29809e8f2488b8687d99e
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-10 10:49:54 +0100

    Create Arrow-functions.js

---
+++ ASE/Worksheet-2/Arrow-functions.js
@@ -0,0 +1,7 @@
+// https://www.learn-js.org/en/Arrow_Functions
+
+const divideByTwo = number => number / 2;
+console.log(divideByTwo(4));
+console.log(divideByTwo(8));
+console.log(divideByTwo(12));
+console.log(divideByTwo(6));
