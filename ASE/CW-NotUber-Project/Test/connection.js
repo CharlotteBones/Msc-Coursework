db45aebd229bc058bf66b1dda6a5d3e48837c0ae
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-14 15:18:29 +0100

    Create connection.js
    I have put <user> <password> inplace of the real username and password.

---
+++ ASE/CW-NotUber-Project/Test/connection.js
@@ -0,0 +1,17 @@
+// I left my test files in here, I used mocha to work out the requests
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise
+
+before(function(done) {
+
+    mongoose.connect('mongodb+srv://<user>:<password>@notuber.8mdfv.mongodb.net/NotUber?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+    mongoose.connection.once('open', () => {
+        console.log('Connection to mongoDB successful');
+        done();
+    }).on('error', (error) => {
+        console.log('Connection failed', error);
+    });
+});
